47a8c5c49da009b497e65c09d3694bd3b6ec612d
laicq <1280060812@qq.com>
2025-07-03 14:25:11 +0800

    lint

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.less'
 import App from './App.tsx'
 
-const rootElement = document.getElementById('root')
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  )
-} else {
-  // 可以根据需要处理找不到 root 元素的情况
-  console.error('Root element not found')
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
